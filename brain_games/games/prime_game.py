e5b332a8ac9800b8d28204237a26012a8750f249
Artem Iliushin <thatsforsure@mail.ru>
2023-05-30 18:43:13 +0800

    fixed brain-prime game

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -4,17 +4,28 @@ import random
 rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def game_conditions():
-
-    numbers = random.randint(3, 100)
+def is_prime(number):
+    if number < 2:
+        return False
 
     divider = 2
 
-    while divider <= numbers / 2:
-        if numbers % divider == 0:
-            correct_answer = 'no'
-        else:
-            correct_answer = 'yes'
+    while divider <= number / 2:
+        if number % divider == 0:
+            return False
+
         divider += 1
+
+    return True
+
+
+def game_conditions():
+
+    numbers = random.randint(1, 100)
+
+    if is_prime(numbers) is True:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
 
     return numbers, correct_answer
